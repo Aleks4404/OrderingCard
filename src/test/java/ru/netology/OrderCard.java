701a64184c3797c13e83810cd80d1598728d315b
Aleksey Efimov <Aleks4404@mail.ru>
2021-06-23 18:35:51 +0400

    Изменение ChromeDriver

--- src/test/java/ru/netology/OrderCard.java
+++ src/test/java/ru/netology/OrderCard.java
@@ -12,7 +12,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class OrderCard {
