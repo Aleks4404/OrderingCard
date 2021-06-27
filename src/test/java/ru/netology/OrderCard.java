b1f95dc0ee08fbdb3aee12e1c7bfd8908917bcd8
Aleksey Efimov <Aleks4404@mail.ru>
2021-06-27 12:44:20 +0400

    Добавление селектора data-test-id и css-класса в файл OrderCard.java

--- src/test/java/ru/netology/OrderCard.java
+++ src/test/java/ru/netology/OrderCard.java
@@ -7,11 +7,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class OrderCard {
@@ -40,9 +38,8 @@ public class OrderCard {
     @Test
     void shouldSubmitAnApplication() {
         driver.get("http://localhost:9999");
-        List<WebElement> elements = driver.findElements(By.className("input__control"));
-        elements.get(0).sendKeys("Иванов Иван");
-        elements.get(1).sendKeys("+79270123456");
+        driver.findElement(By.cssSelector("[data-test-id=name] .input__control")).sendKeys("Иванов Иван");
+        driver.findElement(By.cssSelector("[data-test-id=phone] .input__control")).sendKeys("+79270123456");
         driver.findElement(By.className("checkbox")).click();
         driver.findElement(By.tagName("button")).click();
         String expectedMessage = "  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
@@ -53,22 +50,20 @@ public class OrderCard {
     @Test
     void shouldBeFilledInWithNonValidData() {
         driver.get("http://localhost:9999");
-        List<WebElement> textFields = driver.findElements(By.className("input__control"));
-        textFields.get(0).sendKeys("Ivanov Ivan");
-        textFields.get(1).sendKeys("+79270123456");
+        driver.findElement(By.cssSelector("[data-test-id=name] .input__control")).sendKeys("Ivanov Ivan");
+        driver.findElement(By.cssSelector("[data-test-id=phone] .input__control")).sendKeys("+79270123456");
         driver.findElement(By.className("checkbox")).click();
         driver.findElement(By.tagName("button")).click();
         String expectedMessage = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
-        String actualMessage = driver.findElement(By.className("input__sub")).getText();
+        String actualMessage = driver.findElement(By.cssSelector(".input_invalid .input__sub")).getText();
         assertEquals(expectedMessage, actualMessage);
     }
 
     @Test
     void shouldEnterTheWrongPhoneNumber() {
         driver.get("http://localhost:9999");
-        List<WebElement> textFields = driver.findElements(By.className("input__control"));
-        textFields.get(0).sendKeys("Иванов Иван");
-        textFields.get(1).sendKeys("8927012345");
+        driver.findElement(By.cssSelector("[data-test-id=name] .input__control")).sendKeys("Иванов Иван");
+        driver.findElement(By.cssSelector("[data-test-id=phone] .input__control")).sendKeys("8927012345");
         driver.findElement(By.className("checkbox")).click();
         driver.findElement(By.tagName("button")).click();
         String expectedMessage = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
